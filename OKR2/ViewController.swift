6cb7f0fc5806f7d4d6fa91900e0aeb3ec57aae3b
Vitaliy Golovko <vitaliy.golovko@windmill.com>
2020-06-04 13:04:56 +0300

    Initial Commit

---
+++ OKR2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OKR2
+//
+//  Created by Vitaliy Golovko on 04.06.2020.
+//  Copyright © 2020 VGolovko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
